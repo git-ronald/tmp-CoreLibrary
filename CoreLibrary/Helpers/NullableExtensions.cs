85763df676501532f7431f5f9f3e3aca449fa2ef
git-ronald <inbox.ronald@outlook.com>
2022-02-22 19:28:37 +0100

    Add project files.

---
+++ CoreLibrary/Helpers/NullableExtensions.cs
@@ -0,0 +1,71 @@
+﻿namespace CoreLibrary.Helpers
+{
+    public static class NullableExtensions
+    {
+        public static TValue GetOrThrow<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key) where TKey : notnull
+        {
+            return dictionary.GetOrThrow(key, $"Failed to read key {key}");
+        }
+        public static TValue GetOrThrow<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key, string exceptionMessage) where TKey : notnull
+        {
+            return dictionary.GetOrThrow(key, () => new Exception(exceptionMessage));
+        }
+        public static TValue GetOrThrow<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key, Func<Exception> getException) where TKey : notnull
+        {
+            if (dictionary is null)
+            {
+                throw new ArgumentNullException(nameof(dictionary));
+            }
+
+            if (!dictionary.TryGetValue(key, out TValue? value))
+            {
+                throw getException();
+            }
+            if (value == null)
+            {
+                throw getException();
+            }
+            return value;
+        }
+
+        public static TValue GetOrDefault<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key, TValue defaultValue) where TKey : notnull
+        {
+            if (dictionary is null)
+            {
+                return defaultValue;
+            }
+
+            if (!dictionary.TryGetValue(key, out TValue? value))
+            {
+                return defaultValue;
+            }
+            return value;
+        }
+
+        public static int ForceParseToInt(this string? value) // TODO: rename to ParseToIntValue
+        {
+            if (value is null)
+            {
+                return default;
+            }
+
+            try
+            {
+                return int.Parse(value);
+            }
+            catch
+            {
+                return default;
+            }
+        }
+
+        public static string ForceToString(this object value) // TODO: rename to ToStringValue
+        {
+            if (value == null)
+            {
+                return string.Empty;
+            }
+            return value.ToString() ?? string.Empty;
+        }
+    }
+}
