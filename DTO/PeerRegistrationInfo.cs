1fd8f0281aa9d1a8fc234c9780f18e0d4d4f25eb
git-ronald <inbox.ronald@outlook.com>
2022-03-03 20:22:02 +0100

    Renamed DTO to PeerInterface

--- DTO/PeerRegistrationInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CoreLibrary.DTO
-{
-    public class PeerRegistrationInfo
-    {
-        public Guid PeerId { get; set; }
-        public string PeerName { get; set; } = String.Empty;
-    }
-}
---
+++ PeerInterface/PeerRegistrationInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace CoreLibrary.PeerInterface
+{
+    public class PeerRegistrationInfo
+    {
+        public PeerRegistrationInfo(Guid peerId, string peerName)
+        {
+            PeerId = peerId;
+            PeerName = peerName;
+        }
+
+        public Guid PeerId { get; set; }
+        public string PeerName { get; set; } = string.Empty;
+    }
+}
