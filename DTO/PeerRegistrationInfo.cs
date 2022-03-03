c563cae6a78d4eac02bbef5a0bb39f33b32df651
git-ronald <inbox.ronald@outlook.com>
2022-03-03 18:39:40 +0100

    DTO

---
+++ DTO/PeerRegistrationInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace CoreLibrary.DTO
+{
+    public class PeerRegistrationInfo
+    {
+        public Guid PeerId { get; set; }
+        public string PeerName { get; set; } = String.Empty;
+    }
+}
