ee3c66797a9b6eaba10eba5229caf9c3d6f2beb2
git-ronald <inbox.ronald@outlook.com>
2022-04-03 18:47:46 +0200

    SignalrMessages

---
+++ ConstantValues/SignalrMessages.cs
@@ -0,0 +1,17 @@
+﻿namespace CoreLibrary.ConstantValues
+{
+    public static class SignalrMessages
+    {
+        public const string TestRequest = nameof(TestRequest);
+        public const string TestResponse = nameof(TestResponse);
+
+        public const string RequestPeerRegistrationInfo = nameof(RequestPeerRegistrationInfo);
+        public const string PeerRegistrationInfoResponse = nameof(PeerRegistrationInfoResponse);
+
+        public const string RegistrationUpdate = nameof(RegistrationUpdate);
+        public const string NotifySignOfLife = nameof(NotifySignOfLife);
+
+        public const string PeerRequest = nameof(PeerRequest);
+        public const string PeerResponse = nameof(PeerResponse);
+    }
+}
